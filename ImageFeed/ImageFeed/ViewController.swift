0c4ccd38859b6686a6b02b8c0406b3cd34ed85bd
Petrov Vyacheslav <vyacheslav.petrov@indels.org>
2026-04-14 14:58:50 +0900

    first commit sourcethree

---
+++ ImageFeed/ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Петров Вячеслав on 20.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
